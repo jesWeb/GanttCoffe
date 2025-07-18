bceb1f2b62a30afd16f748c476869bfcc7d6d097
jesus Gutierrez <jesuslgutierrez685@gmail.com>
2025-07-18 12:27:08 -0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+
+function App() {
+
+  return (
+    <>
+      <div className="flex justify-center pt-10 min-h-screen bg-slate-300">
+        <div className="W-4/5 bg-slate-500 rounded shadow-md">
+          <h1 className="text-2xl mb-4 text-center">Gestion de tareas</h1>
+          <div className="w-11/12 mx-auto">
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
